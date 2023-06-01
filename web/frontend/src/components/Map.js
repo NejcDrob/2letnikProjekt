74f357382100bdcaee5d6c6ae353e54a3fe7e075
nikglavic <nik.glavic@gmail.com>
2023-06-01 20:11:12 +0200

    deleted unnecessary files

--- web/frontend/src/components/Map.js
+++ web/frontend/src/components/Map.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function MyMap() {
   const position = [51.505, -0.09];
+  const [roadList, setRoadList] = useState([])
+  useEffect(()=>{
+    const getRoadList=async function(){
+      const res=await fetch("http://localhost:3001/roads")
+      const data=await res.json()
+      setRoadList(data)
+      console.log(data)
+    }
+    getRoadList()
+  }, [])
 
   return (
     <div className='map'>
