0fdb9ef2388ba2ae527c05136b10d2c9faa824a8
Martin Gruber <89796190+NotMort@users.noreply.github.com>
2023-06-01 17:21:44 +0200

    Added Textviews so we can see the data

--- app/app/src/main/java/com/example/drivingapp/ScanFragment.kt
+++ app/app/src/main/java/com/example/drivingapp/ScanFragment.kt
@@ -14,19 +14,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 
-class ScanFragment:Fragment(R.layout.fragment_scan), SensorEventListener, LocationListener {
-    
+class ScanFragment : Fragment(R.layout.fragment_scan), SensorEventListener, LocationListener {
+
     private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
     private lateinit var locationManager: LocationManager
+    private lateinit var accelerometerDataTextView: TextView
+    private lateinit var locationDataTextView: TextView
+
     companion object {
         private const val PERMISSION_REQUEST_CODE = 1001
         private const val MIN_TIME_INTERVAL = 1000L // 1 second
         private const val MIN_DISTANCE_CHANGE = 1.0f // 1 meter
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -43,10 +49,11 @@ class ScanFragment:Fragment(R.layout.fragment_scan), SensorEventListener, Locati
                 PERMISSION_REQUEST_CODE
             )
         } else {
-            // Permission granted, start location updates
-            startLocationUpdates()
+            // Permission granted, start sensors
+            startSensors()
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -64,7 +71,33 @@ class ScanFragment:Fragment(R.layout.fragment_scan), SensorEventListener, Locati
 
         // Get the accelerometer sensor
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        // Initialize the button and set an OnClickListener
+        val buttonStart: Button = view.findViewById(R.id.buttonStart)
+        buttonStart.setOnClickListener {
+            startSensors()
+        }
+
+        // Initialize the TextViews
+        accelerometerDataTextView = view.findViewById(R.id.accelerometerDataTextView)
+        locationDataTextView = view.findViewById(R.id.locationDataTextView)
     }
+
+    private fun startSensors() {
+        startAccelerometer()
+        startLocationUpdates()
+    }
+
+    private fun startAccelerometer() {
+        accelerometer?.let { sensor ->
+            sensorManager.registerListener(
+                this,
+                sensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
     private fun startLocationUpdates() {
         locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
@@ -80,33 +113,27 @@ class ScanFragment:Fragment(R.layout.fragment_scan), SensorEventListener, Locati
                 MIN_DISTANCE_CHANGE,
                 this
             )
+        } else {
+            // Permission not granted, handle the error or request it again
+            // You can show a message to the user or request the permission again
         }
     }
-
 
     override fun onLocationChanged(location: Location) {
         val speed = location.speed // speed in meters/second
         val speedKmPerHour = speed * 3.6 // convert to kilometers/hour
 
-        // Process the speed value as needed
+        val locationData = "Speed: $speedKmPerHour km/h"
+        locationDataTextView.text = locationData
     }
+
     override fun onResume() {
         super.onResume()
-
-        // Register the accelerometer sensor listener
-        accelerometer?.let { sensor ->
-            sensorManager.registerListener(
-                this,
-                sensor,
-                SensorManager.SENSOR_DELAY_NORMAL
-            )
-        }
+        startAccelerometer()
     }
 
     override fun onPause() {
         super.onPause()
-
-        // Unregister the accelerometer sensor listener
         sensorManager.unregisterListener(this)
     }
 
@@ -121,10 +148,9 @@ class ScanFragment:Fragment(R.layout.fragment_scan), SensorEventListener, Locati
                 val y = event.values[1]
                 val z = event.values[2]
 
-                // Process accelerometer values
-                // You can perform any desired logic with the x, y, and z values
+                val accelerometerData = "X: $x\nY: $y\nZ: $z"
+                accelerometerDataTextView.text = accelerometerData
             }
         }
     }
-
 }
