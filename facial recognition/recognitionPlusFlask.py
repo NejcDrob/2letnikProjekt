2e3d179a142b30ac77c3ab0514d30d2ca28f3132
NejcDrob <nejcdrob@gmail.com>
2023-06-04 23:15:32 +0200

    added image testing code

--- facial recognition/recognitionPlusFlask.py
+++ facial recognition/recognitionPlusFlask.py
@@ -33,8 +33,58 @@ def predict_from_image():
         hog_features = hog.compute(resized_image)
         return hog_features
 
+    with open('model_123.pkl', 'rb') as file:
+        model = pickle.load(file)
+
+    def decode_image(base64_string):
+        decoded_data = base64.b64decode(base64_string)
+        np_data = numpy.frombuffer(decoded_data, numpy.uint8)
+        image = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+        return image
     
-    # Return the prediction as JSON
+    if 'image' not in request.json:
+        print('No image part')
+        return jsonify(error='No image part in the request'), 400
+
+    image_base64 = request.json['image']
+    
+    test_image = decode_image(image_base64)
+
+    processed_test_images = []
+    if image is not None:
+        image = cv2.resize(image, (100, 100))
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        processed_test_images.append(gray_image)
+
+    HOG_test_features = []
+    LBP_test_features = []
+    for image in processed_test_images:
+        HOG_features = hog(image)
+        LBP_features = lbp(image)
+        HOG_test_features.append(HOG_features)
+        LBP_test_features.append(LBP_features)
+
+    combined_test_features = []
+    for HOG_feat, LBP_feat in zip(HOG_test_features, LBP_test_features):
+        HOG_flat = HOG_feat.flatten()
+        LBP_flat = LBP_feat.flatten()
+        combined_features = numpy.hstack((HOG_flat, LBP_flat))
+        combined_test_features.append(combined_features)
+
+    test_dataframe = pandas.DataFrame(combined_test_features)
+    test_dataframe.fillna(test_dataframe.mean(), inplace=True)
+    cleaned_test_features = test_dataframe.values.tolist()
+
+    if not cleaned_test_features:
+        print("No test features found.")
+        exit()
+
+    predictions = model.predict(cleaned_test_features)
+
+    for image, prediction in zip(test_image, predictions):
+        print("Prediction:", prediction)
+        cv2.waitKey(0)
+
     return jsonify(prediction=prediction)
     
 if __name__ == '__main__':
